fcc9d186e72ab600248442fc7259c9dc4fa97cd1
technolockkeys <mhmd_halabi99@hotmail.com>
2024-08-25 21:48:30 +0300

    mainlayout updated

--- resources/views/layout/mainlayout.blade.php
+++ resources/views/layout/mainlayout.blade.php
@@ -4,7 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <title>{{$settings['data']['setting']['website']['system_name']}}</title>
+    @if(app()->getLocale() == "en")
+    <title>{{$settings['data']['setting']['website']['system_name']['en']}}</title>
+    @else
+    <title>{{$settings['data']['setting']['website']['system_name']['ar']}}</title>
+    @endif
 
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="{{$settings['data']['setting']['website']['system_logo_icon']}}">
